cd30dfc0aef7bb24f1cfb1d29bd1c02162c67f37
Saswata Bhattacharjee <saswatabhattacharjee.2004@gmail.com>
2024-05-06 14:57:40 +0530

    Basic Movement Done

--- IT Mall Manager/Assets/Scripts/CharacterMovementManager.cs
+++ IT Mall Manager/Assets/Scripts/CharacterMovementManager.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class CharacterMovementManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public VariableJoystick joystick;
+    public CharacterController controller;
+    public Canvas canvas;
+    public bool isJoystick;
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 10f;
+    public Animator anim;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        EnableJoystickInput();
+    }
+    public void EnableJoystickInput()
+    {
+        isJoystick = true;
+        canvas.gameObject.SetActive(true);
+    }
+    private void Update()
+    {
+        if(isJoystick)
+        {
+            Vector3 movementDir = new Vector3(joystick.Direction.x,0.0f,joystick.Direction.y);
+            controller.SimpleMove(movementDir * moveSpeed);
+
+
+
+            Vector3 targetDir = Vector3.RotateTowards(controller.transform.forward, movementDir, rotationSpeed * Time.deltaTime,maxMagnitudeDelta:0.0f);
+
+            controller.transform.rotation = Quaternion.LookRotation(targetDir);
+            if (movementDir.magnitude <= 0.1f)
+            {
+                anim.SetBool("IsMoving", false);
+            }
+            else
+            {
+                anim.SetBool("IsMoving", true);
+            }
+        }
     }
 }
