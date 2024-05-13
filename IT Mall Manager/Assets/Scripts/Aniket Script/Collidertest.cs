a4a74c3a5f1d4a28d8e193047a3f2664bf5c7085
aniket662 <deya7153@gmail.com>
2024-05-14 01:07:11 +0530

    ui updates

--- IT Mall Manager/Assets/Scripts/Aniket Script/Collidertest.cs
+++ IT Mall Manager/Assets/Scripts/Aniket Script/Collidertest.cs
@@ -1,20 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.Diagnostics;
 public class Collidertest : MonoBehaviour
 {
     public bool PlayerInRange=false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float deductAmount=100;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(PlayerInRange){
+            CashMovement.instance.cashReachedPlayer -= deductAmount * Time.deltaTime;
+            CashMovement.instance.cashReachedPlayer = Mathf.Max(CashMovement.instance.cashReachedPlayer,0);
+            CashMovement.instance.UpdateMoneyUi();
+        }
+        if(CashMovement.instance.cashReachedPlayer<=0){
+            Debug.Log("Player is out of cash");
+        }
     }
     void OnTriggerEnter(Collider other){
         if(other.CompareTag("Player")){
