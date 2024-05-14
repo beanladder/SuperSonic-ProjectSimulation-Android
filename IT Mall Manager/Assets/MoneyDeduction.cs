26aec297774c1c965e5cd360bd8ac21b375f9d82
aniket662 <deya7153@gmail.com>
2024-05-15 01:31:57 +0530

    update

--- IT Mall Manager/Assets/MoneyDeduction.cs
+++ IT Mall Manager/Assets/MoneyDeduction.cs
@@ -6,16 +6,13 @@ using UnityEngine;
 public class MoneyDeduction : MonoBehaviour
 {
     public int deductionAmount = 100; // Amount of money to deduct when player is in range
-    public float deductionInterval = 1f;
     public bool playerInRange = false; // Flag to track if player is in range
-    private Coroutine deductionCoroutine;
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             playerInRange = true;
-            //DeductMoney();
-            deductionCoroutine = StartCoroutine(DeductMoneyOverTime());
+            DeductMoney();
         }
     }
 
@@ -24,40 +21,19 @@ public class MoneyDeduction : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
-            if(deductionCoroutine!=null){
-                StopCoroutine(deductionCoroutine);
-            }
         }
     }
-    
-    IEnumerator DeductMoneyOverTime(){
 
-        // Check if player has enough cash to deduct
-            if (PlayerCashCounter.instance.totalCashValue >= deductionAmount)
-            {
-                // Deduct the specified amount
-                PlayerCashCounter.instance.DeductTotalCash(deductionAmount);
-                Debug.Log("Deducted " + deductionAmount + " from player's cash.");
-            }
-            else
-            {
-                Debug.Log("Not enough cash to deduct " + deductionAmount + ".");
-            }
-
-            // Wait for the next deduction interval
-            yield return new WaitForSeconds(deductionInterval);
-        }
-
-    // void DeductMoney()
-    // {
-    //     if (PlayerCashCounter.instance.totalCashValue >= deductionAmount)
-    //     {
-    //         PlayerCashCounter.instance.DeductTotalCash(deductionAmount);
-    //         Debug.Log("Deducted " + deductionAmount + " from player's cash.");
-    //     }
-    //     else
-    //     {
-    //         Debug.Log("Not enough cash to deduct " + deductionAmount + ".");
-    //     }
-    // }
+     void DeductMoney()
+     {
+        if (PlayerCashCounter.instance.totalCashValue >= deductionAmount)
+         {
+             PlayerCashCounter.instance.DeductTotalCash(deductionAmount);
+             Debug.Log("Deducted " + deductionAmount + " from player's cash.");
+         }
+         else
+         {
+             Debug.Log("Not enough cash to deduct " + deductionAmount + ".");
+         }
+     }
 }
