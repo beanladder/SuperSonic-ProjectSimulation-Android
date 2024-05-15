88dc0301f7c7d302dcc2bfeea9abb1ad7b0592cb
Saswata Bhattacharjee <saswatabhattacharjee.2004@gmail.com>
2024-05-16 02:32:45 +0530

    Bal

--- IT Mall Manager/Assets/Scripts/AI/Shelf.cs
+++ IT Mall Manager/Assets/Scripts/AI/Shelf.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Shelf : MonoBehaviour
 {
-
     public static Shelf instance;
+
     // Enum to represent the different types of shelves
     public enum ShelfType { CPU, RAM, Motherboard }
     public int productCount;
@@ -13,7 +13,36 @@ public class Shelf : MonoBehaviour
     // Variable to hold the type of this shelf
     public ShelfType shelfType;
 
-    private void Awake() {
+    // Array to hold references to the spawn points
+    public Transform[] spawnPoints;
+
+    // Prefabs for RAM, CPU, and Motherboard
+    public GameObject ramPrefab;
+    public GameObject cpuPrefab;
+    public GameObject motherboardPrefab;
+
+    private void Awake()
+    {
         instance = this;
+
+        // Find all spawn points that are children of this shelf (excluding the shelf itself)
+        List<Transform> childTransforms = new List<Transform>();
+        foreach (Transform child in transform)
+        {
+            if (child != transform) // Exclude the parent shelf object
+            {
+                childTransforms.Add(child);
+            }
+        }
+        spawnPoints = childTransforms.ToArray();
+    }
+
+    // Detect collision with the player
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player collided with shelf of type: " + shelfType);
+        }
     }
 }
