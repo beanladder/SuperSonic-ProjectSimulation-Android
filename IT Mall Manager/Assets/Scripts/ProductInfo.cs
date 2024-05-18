1221ac54ff67acaae566002fae4c055f09f68b0c
Saswata Bhattacharjee <saswatabhattacharjee.2004@gmail.com>
2024-05-18 14:04:00 +0530

    Updated product info only for player

--- IT Mall Manager/Assets/Scripts/ProductInfo.cs
+++ IT Mall Manager/Assets/Scripts/ProductInfo.cs
@@ -2,24 +2,49 @@ using UnityEngine;
 
 public class ProductInfo : MonoBehaviour
 {
-    public int MaxProductsOfEachType;
+    public int MaxProductsOfEachType=1;
     public int CpuNum, MBNum, RamNum;
+
     public GameObject ramPrefab;
     public GameObject cpuPrefab;
     public GameObject motherboardPrefab;
+
     public static ProductInfo instance;
 
     private void Awake()
     {
         instance = this;
     }
+
     private void Start()
     {
         CpuNum = MBNum = RamNum = MaxProductsOfEachType;
     }
+
+    public void IncreaseMaxCapacity()
+    {
+        MaxProductsOfEachType++;
+
+        // Adjust existing stock if needed (optional)
+        if (CpuNum < MaxProductsOfEachType)
+        {
+            CpuNum++;
+        }
+        if (MBNum < MaxProductsOfEachType)
+        {
+            MBNum++;
+        }
+        if (RamNum < MaxProductsOfEachType)
+        {
+            RamNum++;
+        }
+    }
+
+   
+
     private void Update()
     {
-        if(CpuNum ==0 && RamNum ==0 && MBNum == 0)
+        if (CpuNum == 0 && RamNum == 0 && MBNum == 0)
         {
             Destroy(gameObject);
         }
